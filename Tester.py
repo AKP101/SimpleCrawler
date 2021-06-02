b840bbeada124d1654ba69f49201a84acf24a3ba
A Peer <47980558+AKP101@users.noreply.github.com>
2021-06-01 21:12:49 -0400

    Update Tester.py

--- Tester.py
+++ Tester.py
@@ -1,40 +1,25 @@
-from selenium import webdriver
-from bs4 import BeautifulSoup
+from selenium import webdriver #Selenium can be used for testing with chrome. 
+from bs4 import BeautifulSoup # Need to import from bs4 not BeautifulSoup bc that 'bs4' is the new version
 import pandas as pd
 
 products=[] #List to store name of the product
 prices=[] #List to store price of the product
 ratings=[] #List to store rating of the product
-driver = webdriver.Chrome()
-driver.get('https://www.flipkart.com/laptops/~buyback-guarantee-on-laptops-/pr?sid=6bo%2Cb5g&uniqBStoreParam1=val1&wid=11.productCard.PMU_V2')
+driver = webdriver.Chrome() # can specify chrome's location but not necessary.
+driver.get('https://www.flipkart.com/laptops/~buyback-guarantee-on-laptops-/pr?sid=6bo%2Cb5g&uniqBStoreParam1=val1&wid=11.productCard.PMU_V2') # as per tutorial
 
 content = driver.page_source
-soup = BeautifulSoup(content, 'html.parser')
-
+soup = BeautifulSoup(content, 'html.parser') #'html parse' tells us which parser to use (recommended?)
+ # Using fina and find all to gather all the data from the html containers. 
 for a in soup.findAll('a', href=True, attrs={'class':'_1fQZEK'}): #href=True, 
-    name=a.find('div', attrs={'class':'_4rR01T'})
-    print(name.text)
-    price=a.find('div', attrs={'class' :'_30jeq3 _1_WHN1'})
-    print(price.text)
-##    rating=a.find('div', attrs={'class':'_3LWZ1K'})
-##    print(rating)
+    name=a.find('div', attrs={'class':'_4rR01T'}) # attrs is attributes?
+    # print(name.text)
+    price=a.find('div', attrs={'class' :'_30jeq3 _1_WHN1'}) # div is the command like h1 or a etc...
+    # print(price.text)
     products.append(name.text)
     prices.append(price.text)
-##    ratings.append(rating.text)
-print(products)
-print(prices)
-print(ratings)
-df = pd.DataFrame({'Product Name':products,'Price':prices}) #,'Rating':ratings
+# print(products)
+# print(prices)
+df = pd.DataFrame({'Product Name':products,'Price':prices}) #Puts this into an excel file
 df.to_csv('products.csv', index=False, encoding='utf-8')
-#C:\Users\AnikaP\AppData\Local\Programs\Python\Python39
-##import time
-##from selenium import webdriver
-##
-##driver = webdriver.Chrome()  # Optional argument, if not specified will search path.
-##driver.get('http://www.google.com/');
-##time.sleep(5) # Let the user actually see something!
-##search_box = driver.find_element_by_name('q')
-##search_box.send_keys('ChromeDriver')
-##search_box.submit()
-##time.sleep(5) # Let the user actually see something!
-##driver.quit()
+
