c4e689868d06181c0329d406874695afc4f42aee
A Peer <47980558+AKP101@users.noreply.github.com>
2021-06-01 21:14:02 -0400

    Update web-s2.py

--- web-s2.py
+++ web-s2.py
@@ -4,25 +4,20 @@ import pandas as pd
 
 products=[] #List to store name of the product
 prices=[] #List to store price of the product
-ratings=[] #List to store rating of the product
 driver = webdriver.Chrome()
 driver.get('https://www.flipkart.com/home-kitchen/kitchen-appliances')
 
 content = driver.page_source
 soup = BeautifulSoup(content, 'html.parser')
 
-for a in soup.findAll('div', attrs={'class':'_4ddWXP'}): #href=True, 
+for a in soup.findAll('div', attrs={'class':'_4ddWXP'}):
     name=a.find('a', attrs={'class':'s1Q9rs'})
-    print(name.text)
+#     print(name.text)
     price=a.find('div', attrs={'class' :'_30jeq3'})
-    print(price.text)
-##    rating=a.find('div', attrs={'class':'_3LWZ1K'})
-##    print(rating)
+#     print(price.text)
     products.append(name.text)
     prices.append(price.text)
-##    ratings.append(rating.text)
-print(products)
-print(prices)
-print(ratings)
+# print(products)
+# print(prices)
 df = pd.DataFrame({'Product Name':products,'Price':prices}) #,'Rating':ratings
 df.to_csv('products2.csv', index=False, encoding='utf-8')
